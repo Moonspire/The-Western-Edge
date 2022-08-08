70f2030da76e3518292181d803e1b8cda6bc99ca
Moonspire <21323568+Moonspire@users.noreply.github.com>
2022-08-07 23:12:13 -0600

    Changed PotionApplyEffectModification.java from Overwrite to Inject

--- Workspace/src/main/java/com/github/moonspire/thewesternedge/mixin/PotionApplyEffectModification.java
+++ Workspace/src/main/java/com/github/moonspire/thewesternedge/mixin/PotionApplyEffectModification.java
@@ -1,11 +1,12 @@
 package com.github.moonspire.thewesternedge.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.LivingEntity;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.advancements.CriteriaTriggers;
@@ -18,43 +19,45 @@ import net.minecraft.world.level.gameevent.GameEvent;
 
 @Mixin(PotionItem.class)
 public class PotionApplyEffectModification {
-	@Overwrite(remap = false)
-	public ItemStack finishUsingItem(ItemStack p_42984_, Level p_42985_, LivingEntity p_42986_) {
-		ItemStack emptyBottle = new ItemStack(Items.GLASS_BOTTLE);
-		emptyBottle.getOrCreateTag().putDouble("CustomModelData", p_42984_.getOrCreateTag().getDouble("CustomModelData"));
-		Player player = p_42986_ instanceof Player ? (Player)p_42986_ : null;
-		if (player instanceof ServerPlayer) {
-		 CriteriaTriggers.CONSUME_ITEM.trigger((ServerPlayer)player, p_42984_);
+	@Inject(at = @At("HEAD"), method = "finishUsingItem(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/LivingEntity;)Lnet/minecraft/world/item/ItemStack;", cancellable = true, remap = false)
+	public void finishUsingItem(ItemStack itemStack, Level level, LivingEntity livingEntity, CallbackInfoReturnable<ItemStack> callback) {
+		if (itemStack.getOrCreateTag().getDouble("CustomModelData") != 0) {
+			ItemStack emptyBottle = new ItemStack(Items.GLASS_BOTTLE);
+			emptyBottle.getOrCreateTag().putDouble("CustomModelData", itemStack.getOrCreateTag().getDouble("CustomModelData"));
+			Player player = livingEntity instanceof Player ? (Player)livingEntity : null;
+			if (player instanceof ServerPlayer) {
+			 CriteriaTriggers.CONSUME_ITEM.trigger((ServerPlayer)player, itemStack);
+			}
+			
+			if (!level.isClientSide) {
+			 for(MobEffectInstance mobeffectinstance : PotionUtils.getMobEffects(itemStack)) {
+			    if (mobeffectinstance.getEffect().isInstantenous()) {
+			       mobeffectinstance.getEffect().applyInstantenousEffect(player, player, livingEntity, mobeffectinstance.getAmplifier(), 1.0D);
+			    } else {
+			       livingEntity.addEffect(new MobEffectInstance(mobeffectinstance));
+			    }
+			 }
+			}
+			
+			if (player != null) {
+			 player.awardStat(Stats.ITEM_USED.get(itemStack.getItem()));
+			 if (!player.getAbilities().instabuild) {
+			    itemStack.shrink(1);
+			 }
+			}
+			
+			if (player == null || !player.getAbilities().instabuild) {
+			 if (itemStack.isEmpty()) {
+			    callback.setReturnValue(emptyBottle);
+			 }
+			
+			 if (player != null) {
+			    player.getInventory().add(emptyBottle);
+			 }
+			}
+			
+			level.gameEvent(livingEntity, GameEvent.DRINKING_FINISH, livingEntity.eyeBlockPosition());
+			callback.setReturnValue(itemStack);
 		}
-		
-		if (!p_42985_.isClientSide) {
-		 for(MobEffectInstance mobeffectinstance : PotionUtils.getMobEffects(p_42984_)) {
-		    if (mobeffectinstance.getEffect().isInstantenous()) {
-		       mobeffectinstance.getEffect().applyInstantenousEffect(player, player, p_42986_, mobeffectinstance.getAmplifier(), 1.0D);
-		    } else {
-		       p_42986_.addEffect(new MobEffectInstance(mobeffectinstance));
-		    }
-		 }
-		}
-		
-		if (player != null) {
-		 player.awardStat(Stats.ITEM_USED.get(p_42984_.getItem()));
-		 if (!player.getAbilities().instabuild) {
-		    p_42984_.shrink(1);
-		 }
-		}
-		
-		if (player == null || !player.getAbilities().instabuild) {
-		 if (p_42984_.isEmpty()) {
-		    return emptyBottle;
-		 }
-		
-		 if (player != null) {
-		    player.getInventory().add(emptyBottle);
-		 }
-		}
-		
-		p_42985_.gameEvent(p_42986_, GameEvent.DRINKING_FINISH, p_42986_.eyeBlockPosition());
-		return p_42984_;
 	}
 }
