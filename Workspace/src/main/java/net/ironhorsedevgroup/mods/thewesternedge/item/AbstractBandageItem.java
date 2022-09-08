7104e875eed7a71c8ca0a01f2542ae9004d38e20
Moonspire <21323568+Moonspire@users.noreply.github.com>
2022-09-08 00:57:15 -0600

    Shifted most of bandage code from GauzeStripItem class to AbstractBandageItem class

--- Workspace/src/main/java/net/ironhorsedevgroup/mods/thewesternedge/item/AbstractBandageItem.java
+++ Workspace/src/main/java/net/ironhorsedevgroup/mods/thewesternedge/item/AbstractBandageItem.java
@@ -3,6 +3,7 @@ package net.ironhorsedevgroup.mods.thewesternedge.item;
 import com.mojang.blaze3d.shaders.Effect;
 import net.ironhorsedevgroup.mods.thewesternedge.init.TWEMobEffects;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
@@ -10,6 +11,9 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUtils;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.Level;
 
 public class AbstractBandageItem extends Item implements ItemRightClickTrigger {
     public AbstractBandageItem(Item.Properties properties) {
@@ -36,12 +40,41 @@ public class AbstractBandageItem extends Item implements ItemRightClickTrigger {
         return 0;
     }
 
+    @Override
+    public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
+        if (itemstack.getItem() instanceof AbstractBandageItem bandageitem) {
+            return bandageitem.applyGauzeStripEffect(entity, itemstack);
+        }
+        return itemstack;
+    }
+
+    @Override
     public int mobInteract(InteractionHand hand, Entity entity, Player player) {
-        if (entity instanceof LivingEntity livingentity) {
-            applyGauzeStripEffect(livingentity, player.getItemInHand(hand), this.getCooldown(), this.getStrength(), this.getEffectDuration(), this.getMobEffect());
+        ItemStack itemstack = player.getItemInHand(hand);
+        if (itemstack.getItem() instanceof AbstractBandageItem bandageitem && entity instanceof LivingEntity livingentity) {
+            bandageitem.applyGauzeStripEffect(livingentity, itemstack);
             return this.getCooldown();
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public int getUseDuration(ItemStack itemstack) {
+        if (itemstack.getItem() instanceof AbstractBandageItem bandageitem) {
+            return bandageitem.getUseDuration();
         }
         return 0;
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack itemstack) {
+        return UseAnim.DRINK;
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
+        return ItemUtils.startUsingInstantly(world, entity, hand);
     }
 
     public ItemStack applyGauzeStripEffect(LivingEntity entity, ItemStack itemstack, int cooldown, int strength, int duration, MobEffect effect) {
@@ -55,6 +88,13 @@ public class AbstractBandageItem extends Item implements ItemRightClickTrigger {
             itemstack.shrink(1);
         }
         entity.addEffect(new MobEffectInstance(effect, duration, strength));
+        return itemstack;
+    }
+
+    public ItemStack applyGauzeStripEffect(LivingEntity entity, ItemStack itemstack) {
+        if (itemstack.getItem() instanceof AbstractBandageItem bandageitem) {
+            return applyGauzeStripEffect(entity, itemstack, bandageitem.getCooldown(), bandageitem.getStrength(), bandageitem.getEffectDuration(), bandageitem.getMobEffect());
+        }
         return itemstack;
     }
 }
--- Workspace/src/main/java/net/ironhorsedevgroup/mods/thewesternedge/item/GauzeStripItem.java
+++ Workspace/src/main/java/net/ironhorsedevgroup/mods/thewesternedge/item/GauzeStripItem.java
@@ -1,14 +1,7 @@
-
 package net.ironhorsedevgroup.mods.thewesternedge.item;
 
 import net.ironhorsedevgroup.mods.thewesternedge.init.TWETabs;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.entity.LivingEntity;
 
 public class GauzeStripItem extends AbstractBandageItem {
 	public GauzeStripItem() {
@@ -18,36 +11,5 @@ public class GauzeStripItem extends AbstractBandageItem {
 	@Override
 	public int getEffectDuration() {
 		return 240;
-	}
-
-	@Override
-	public UseAnim getUseAnimation(ItemStack itemstack) {
-		return UseAnim.DRINK;
-	}
-
-	@Override
-	public int getUseDuration(ItemStack itemstack) {
-		return this.getUseDuration();
-	}
-
-	@Override
-	public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
-		return ItemUtils.startUsingInstantly(world, entity, hand);
-	}
-
-	@Override
-	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
-		return this.applyGauzeStripEffect(entity, itemstack, this.getCooldown(), this.getStrength(), this.getEffectDuration(), this.getMobEffect());
-	}
-
-	@Override
-	public int mobInteract(InteractionHand hand, Entity entity, Player player) {
-		if (entity instanceof LivingEntity livingentity) {
-			ItemStack itemstack = player.getItemInHand(hand);
-			this.applyGauzeStripEffect(livingentity, itemstack, this.getCooldown(), this.getStrength(), this.getEffectDuration(), this.getMobEffect());
-			return this.getCooldown();
-		} else {
-			return 0;
-		}
 	}
 }
